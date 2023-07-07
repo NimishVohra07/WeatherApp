e85936424abd7bdcaeeb90a2075222323ead8100
Nimish Vohra <136614864+NimishVohra07@users.noreply.github.com>
2023-07-07 20:09:02 +0530

    Add files via upload

---
+++ Weather app (Project 3)/weatherapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Temp from "/Users/nimishvohra/Desktop/Weather app (Project 3)/weatherapp/src/components/temp.js"
+ 
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
